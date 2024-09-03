9751202f1f1692d2601d879b884ff73044216aa5
Danilo <danilohsaraiva@gmail.com>
2024-09-03 15:54:40 -0300

    Alterando as resoluções necessárias para participar da seleção 2024

--- README.md
+++ README.md
@@ -1,27 +1,11 @@
 # desafio_ribeiraopreto2024
-Etapa teste do processo seletivo (Estágio)
+## Etapa teste do processo seletivo (Estágio) -📚 Processo [Target](https://www.targetsistemas.com.br/sobre)
+## Linguagem utilizada: C
 
-📚 Processo [Target](https://www.targetsistemas.com.br/sobre)
 
-Consiste na resolução lógica e algorítmica de algumas questões, que neste repositório estão no arquivo: desafio_ribeirao_preto_2024 e neste arquivo README.md.
 
-Como a proposta é não usar de funções prontas, decidi por resolve-los utilizando da linguagem C.
+#### OutPut 
+![alt text](image.png) 🌵
 
-### ✏ Resolução questão 4:
-
->Supondo que são 3 salas: A, B e C<br>
->Supondo interruptores X1, X2 e X3
-
-- **Ligaria X1 e esperaria um tempo significativo...**
-- **Apagaria X1 e ligaria X2**
-- **Entraria na sala A...**
-- **Lâmpada  ligada X2 = A**
-- **Lâmpada quente e apagada X1 = A**
-- **Lâmpada apagada e fria, então A != X1 e X2 então X3 = A**
-
-Dependendo do resultado, caso:<br>
-
-|Lâmpada Ligada<br>(X2 = A)|Lâmpada apagada e quente<br>(X1 = A)|Lâmpada apagada e fria<br>(X3 = A)|
-|-----|----|----|
-|Entraria na sala B , caso a lâmpada esteja quente B = X1, automaticamente C = X3 e vice-versa se fria o contrário.<br>|Entraria na sala B, se estivesse acesa a X2 = B e X3 = C, caso apagada X2 = C e X3 = B|Se sala B estiver ligada e fria B = X2, caso apagada  C = X1 e se quente B = X1 e C = X2.|
-
+#### ✏ Resolução questão 5:
+![alt text](case_lampadas-1.jpg)
--- desafio_ribeirao_preto_2024.c
+++ desafio_ribeirao_preto_2024.c
@@ -5,18 +5,6 @@
 int main(int argc, char const *argv[])
 {
     printf("\n*************** QUESTAO 1 ***************\n");
-
-    int indice = 13;
-    int soma = 0;
-    int contador = 0;
-
-    while(contador < indice) {
-        contador = contador + 1;
-        soma = soma + contador;
-    }
-    printf("Resposta da primeira questao, soma eh igual a :> %d\n", soma);
-
-    printf("\n*************** QUESTAO 2 ***************\n");
     
     int valorAtual = 0;
     int vetorFibonacci[2] = {0 , 1};
@@ -45,47 +33,41 @@ int main(int argc, char const *argv[])
         
     (statusBuscaFibonacci == true) ? printf("Resposta: O numero %d faz parte da Sequencia Fabonacci\n", valorInformado ): printf("Resposta: O numero %d nao faz parte da Sequencia Fabonacci\n", valorInformado);
 
+    printf("\n*************** QUESTAO 2 ***************\n");
+    printf("Informe uma palavra para averiguar a existencia de caracteres A/a:> ");
+    char palavra[50];
+    int contador = 0;
+    char letra = 'A';
+    scanf("%s", &palavra);
+
+    for (int i = 0; i < strlen(palavra); i++) {
+        if(letra == toupper(palavra[i])) {
+            contador++;
+        }
+    }
+
+    printf("Quantidade de letras A/a na palavra informada eh:> %d\n", contador);
+
     printf("\n*************** QUESTAO 3 ***************\n");
+
+    int indice = 12;
+    int soma = 0;
+    contador = 1;
+
+    while(contador < indice) {
+        contador = contador + 1;
+        soma = soma + contador;
+    }
+    printf("Resposta da primeira questao, soma eh igual a :> %d\n", soma);
+
+    printf("\n*************** QUESTAO 4 ***************\n");
     printf("Resolucao:\n");
     printf("a) 1, 3, 5, 7 , [?] | Resposta: [?] = 9\n");
     printf("b) 2, 4, 8, 16, 32, 64, [???] | Resposta: [???] = 128\n");
     printf("c) 0, 1, 4, 9, 16, 25, 36, [??] | Resposta: 49\n");
     printf("d) 4, 16, 36, 64, [???] | Resposta: 100\n");
     printf("e) 1, 1, 2, 3, 5, 8, [??] | Resposta: 13\n");
-    printf("f) 2, 10, 12, 16, 17, 18, 19, [??] | Resposta: 200\n");
-
-    printf("\n*************** QUESTAO 4 ***************\n");
-    printf("Resposta no READMD.md\n");
-
-    printf("\n*************** QUESTAO 5 ***************\n");
-
-    char palavra[150];
-    int quantidadeCaracteres = 0;
-    int quantidadeNecessariasParaInverterString = 0;
-    int contadorRegressivo = 0;
-    char utilTroca[1];
-
-    printf("Informe uma palavra de ate 150 caracteres:> ");
-    scanf("%s", &palavra);
-
-    quantidadeCaracteres = strlen(palavra);
-
-    contadorRegressivo = quantidadeCaracteres - 1;
-
-    if(quantidadeCaracteres % 2 != 0) {
-        quantidadeNecessariasParaInverterString = (quantidadeCaracteres - 1) / 2;
-    } else {
-        quantidadeNecessariasParaInverterString = (quantidadeCaracteres) / 2;
-    }
-
-    for (int i = 0; i < quantidadeNecessariasParaInverterString; i++) {
-        utilTroca[0] = palavra[i];
-        palavra[i] = palavra[contadorRegressivo];
-        palavra[contadorRegressivo] = utilTroca[0];
-        contadorRegressivo--;
-    }
-
-    printf("Resposta: Palavra de invertida:> %s\nObs:. eh o mesmo enderece de memoria\n", palavra);
+    printf("f) 2, 10, 12, 16, 17, 18, 19, [???] | Resposta: 200\n");
 
     return 0;
 }
